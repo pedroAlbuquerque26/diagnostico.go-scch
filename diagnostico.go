ba8bbf5917e47a0eefdc8cd7d7cf6d6fee8248b0
pedroAlbuquerque26 <pedro.26032009@gmail.com>
2026-02-16 10:52:16 -0300

    Add files via upload

---
+++ diagnostico.go
@@ -0,0 +1,15 @@
+package main 
+import "fmt"
+
+	//Na falha A é possível verificar uma falha de confiabilidade com o programa que está sendo executado, pois o programa deve ser capaz de se adaptar a qualquer tipo de adversidade no servidor, e como esse não foi o caso, consequentmente pela alta demora do funcionamento do site a grande desistência de usuários
+
+	//Na falha B, o site não suportou o pico de acessos, demonstrando falta de escalabilidade. Além disso, houve falha na segurança, o que resultou na exposição de dados pessoais.
+	//Na falha C, problemas de comunicação entre os desenvolvedores dificultaram a correção dos bugs, tornando a manutenção do sistema mais complexa.
+
+func main() {
+
+	fmt.Println("Ambiente configurado e desafio analisado!")
+
+	fmt.Scanln()
+
+}
